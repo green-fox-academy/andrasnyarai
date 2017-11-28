b6a1508cf3399a88a0533b3cacd4e7cd279714b5
andrasnyarai <andrasnyarai@gmail.com>
2017-11-28 17:36:14 +0100

    fixed

--- week-02/d02/sum_all.py
+++ week-02/d02/sum_all.py
@@ -2,13 +2,9 @@
 #   with the following content: `[3, 4, 5, 6, 7]`
 # - Print the sum of the elements in `ai`
 
-
 ai = [3, 4, 5, 6, 7]
 
-sum = 0
 
-for i in range(int(len(ai))):
-    sum += ai[i]
+ai = range(3, 8)
 
-print("The list :" + str(ai))
-print("The sum of the elements of the list is :" + str(sum))
+print(sum(ai))
