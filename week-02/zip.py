2379d8657c988aa42597d172155092fc3099862c
andrasnyarai <andrasnyarai@gmail.com>
2017-11-30 02:14:22 +0100

    last week corrigations2

--- week-02/zip.py
+++ week-02/zip.py
@@ -1,14 +1,2 @@
-''' x = [1, 2, 3]
-y = [4, 5, 6]
-zipped = zip(x, y)
-list(zipped)
-print(zipped)
-print(zipped['4']) '''
-
-
 def make_green(name):
-  new_name = "Green " + name
-  return new_name
-
-''' name = make_green("Tojas")
-greet_by_name(name) '''
+  return "Green" + name
