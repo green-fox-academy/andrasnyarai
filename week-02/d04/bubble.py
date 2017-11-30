456e8ed634530e096b7628d65e8b6594d902425c
andrasnyarai <andrasnyarai@gmail.com>
2017-11-30 21:01:04 +0100

    bubble sort

--- week-02/d04/bubble.py
+++ week-02/d04/bubble.py
@@ -3,17 +3,27 @@
 # Make a second boolean parameter, if it's true sort that list descending
 
 my_list = [12, 5, 13, 8, 9, 65]
+# switch
+descent = False
 
 def bubble(bad_list):
     length = len(bad_list) - 1
     sorted = False
 
-    while not sorted:
+    while not sorted and descent is False:
         sorted = True
         for i in range(length):
             if bad_list[i] > bad_list[i+1]:
                 sorted = False
                 bad_list[i], bad_list[i+1] = bad_list[i+1], bad_list[i]
+    while not sorted and descent is True:
+        sorted = True
+        for i in range(length):
+            if bad_list[i] < bad_list[i+1]:
+                sorted = False
+                bad_list[i], bad_list[i+1] = bad_list[i+1], bad_list[i]
+    else:
+            return bad_list
 
-bubble(my_list)
-print(my_list)
+print(bubble(my_list))
+
