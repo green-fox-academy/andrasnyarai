25bec2e364faa58b9ed745bc30102c3b252f81a4
andrasnyarai <andrasnyarai@gmail.com>
2017-11-30 21:24:56 +0100

    searching str index

---
+++ week-02/d04/substr.py
@@ -0,0 +1,15 @@
+# Create a function that takes two strings as a parameter
+# Returns the starting index where the second one is starting in the first one
+# Returns -1 if the second string is not in the first one
+
+
+input = "this is what I'm searching in"
+input_word = "searching"
+
+def searching(sentence, word):
+    s_index = sentence.find(word, 1)
+    return s_index
+    
+
+
+print(searching(input, input_word))
