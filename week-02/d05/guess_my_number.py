cc8a868dd94167ce836890b90a46cf8c1b0494cd
andrasnyarai <andrasnyarai@gmail.com>
2017-12-02 21:21:38 +0100

    working game

--- week-02/d05/guess_my_number.py
+++ week-02/d05/guess_my_number.py
@@ -1,11 +1,30 @@
-Number = 52
-More = "more"
-less = "less"
+import random
+
+range_user = input("Give me the maximum value of the random number: ")
+r = random.randint(1, int(range_user))
+win = False
 lives = 5
-while i in range(lives)
-    answer = input("guess")
-    if int(answer) == Number, and lives > 0:
-        print("congrat")
-    elif int(answer) 
+print("I've the number between 1-" + str(range_user) + ". You have " + str(lives) + " lives.")
+
+while lives > 0 and not win:
+    guess = input("Make your Guess: ")
+    if int(guess) == r:
+        win = True
+        print("Congratulations!")
+    if int(guess) > r:
+        lives -= 1
+        if lives == 0:
+            print("You are dead!")
+        else:
+            print("Too high, you have " + str(lives) + " lives left.")
+    if int(guess) < r:
+        lives -= 1
+        if lives == 0:
+            print("You are dead!")
+        else:
+            print("Too low, you have " + str(lives) + " lives left.")
+
+
+
 
 
