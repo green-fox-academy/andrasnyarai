76938f5fbed310e16fd9f3cf364fed014f949820
andrasnyarai <andrasnyarai@gmail.com>
2018-01-10 12:10:18 +0100

    one exec add

---
+++ week-07/d03/queryselector-1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let king = document.getElementById('b325');
+console.log('king', king);
+
+let conceited = document.querySelector('.b326').textContent
+alert(conceited)
+
+let businessLamp = document.querySelectorAll('.big')
+console.log(businessLamp)
+
+let conceitedKing = document.querySelectorAll('section > div')
+for (let index = 0; index < conceitedKing.length; index++) {
+    alert(conceitedKing[index])
+}
+let noBusiness = document.querySelectorAll('div');
+for (let i = 0; i < noBusiness.length; i++) {
+    console.log(noBusiness[i])
+}
+let allBizniss = document.querySelector('p');
+alert(allBizniss)
