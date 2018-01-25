a71731669f162ace2197f8b32ba04b6ac75b5251
andrasnyarai <andrasnyarai@gmail.com>
2018-01-25 16:52:15 +0100

    (feat) exec done making server to client
    channeling the yoda speak to another api on the internet with npm xmlhttprequest

--- week-09/d04/index.js
+++ week-09/d04/index.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 const app = express()
 
 app.use(bodyParser.json());
@@ -60,6 +62,134 @@ app.post('/dountil/:what', function (req, res) {
         res.json({"result": fact})
     }
 })
+
+app.post('/arrays', function (req, res) {
+    let method = req.body.what
+    let arr = req.body.numbers
+    if (typeof method === 'undefined' || typeof arr === 'undefined') {
+        res.json({"error": "Please provide what to do with the numbers!"})
+    } else {
+        if (method === 'sum') {
+            arr = arr.reduce((a, b) => a + b, 0)
+            res.json({"result": arr})
+        } else if (method === 'multiply') {
+            arr = arr.reduce((a, b) => a * b, 1)
+            res.json({"result": arr})
+        } else if (method === 'double') {
+            let newArr = arr.map(function (item) {
+                return item * 2
+            })
+            res.json({"result": newArr})       
+        }
+    }
+})
+
+app.post('/sith', function (req, res) {
+    let sentence = req.body.text
+    if (typeof sentence === 'undefined' || sentence === '') {
+        res.json({"error": "Feed me some text you have to, padawan young you are. Hmmm."})
+    } else {
+        let httpRequest = new XMLHttpRequest();
+        
+        httpRequest.open('GET', 'https://yoda.p.mashape.com/yoda?sentence=' + sentence);
+        httpRequest.setRequestHeader("X-Mashape-Key", "kD0uEpXfdLmshbBtzPxApb2C0lWAp1SYQo9jsn5BqjtXfvh4bu");
+        httpRequest.setRequestHeader("Accept", "text/plain");
+        
+        httpRequest.onload = function() {
+            console.log(httpRequest.responseText)
+            res.json({"sith_text": httpRequest.responseText})
+        }
+        httpRequest.send();
+    }
+})
+
+app.post('/translate', function (req, res) {
+    let text = req.body.text
+    let lang = req.body.lang
+    if (typeof text === 'undefined' || typeof lang === 'undefined') {
+        res.json({"error": "I can't translate that!"})        
+    } else {
+        if (lang === 'hu') {
+
+            let isVovel = function(char){
+                if (char == '' || char.length > 1) {
+                  throw Error('Please, give me one character')
+                } else {
+                  return 'aeiouéáőűöüóí'.indexOf(char) > -1;
+                }
+            }
+
+            text = text.split('');
+            let teve = '';
+            text.forEach(function(char){
+              if (isVovel(char)){
+                teve += char + 'v'+ char;
+              } else {
+                  teve += char
+              }
+            });
+
+            res.json({
+                "translated": teve,
+                "lang": "teve"
+            })
+
+        } else if (lang === 'en') {
+
+            let isVovel = function(char){
+                if (char == '' || char.length > 1) {
+                  throw Error('Please, give me one character')
+                } else {
+                  return 'aeiou'.indexOf(char) > -1;
+                }
+            }
+
+            text = text.split('');
+            let ubi = '';
+            text.forEach(function(char){
+              if (isVovel(char)){
+                ubi += 'ub'+ char;
+              } else {
+                  ubi += char
+              }
+            });
+
+            res.json({
+                "translated": ubi,
+                "lang": "ubi"
+            })
+
+        }
+    }
+})
+
+
+// ub
+
+// Create a POST /translate endpoint
+// That receives a simple json object:
+// {
+//   "text": "Ez egy példamondat. Remélem célbatalál.",
+//   "lang": "hu"
+// }
+// And responds with the translated text and its language:
+// {
+//   "translated": "Evez evegy pévéldavamovondavat. Revemévélevem cévélbavavtavalávál.",
+//   "lang": "teve"
+// }
+// from hungarian translate to teve language
+// from english translate to any form of gibberish:
+// https://www.wikiwand.com/en/Gibberish_(language_game)
+// if the input doesnt contain the text (and the language) or its empty, respond with:
+// {
+//   "error": "I can't translate that!"
+// }
+
+
+
+
+
+
 
 app.listen('8080', function() {
     console.log('server is on the fly...')
